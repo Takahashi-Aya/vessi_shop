8b3fe681d6537fc8732e9cbaf55d69287466d3f8
Takahashi-Aya <music.0802.diat@gmail.com>
2024-03-08 02:12:22 +0900

    nav,header１部まで

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // ハンバーガーメニューの開閉
 document.querySelector('.header-btn').addEventListener('click', function(){
     this.classList.toggle('active');
-    document.querySelector('.header-nav').classList.toggle('active');
+    document.querySelector('.global__nav__list').classList.toggle('active');
     });
