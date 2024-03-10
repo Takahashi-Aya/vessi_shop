f5e97dc934e58efa1802d444faa22fe69ca88cab
Takahashi-Aya <music.0802.diat@gmail.com>
2024-03-11 04:31:10 +0900

    Charm途中まで

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 // ハンバーガーメニューの開閉
 document.querySelector('.header-btn').addEventListener('click', function(){
     this.classList.toggle('active');
-    document.querySelector('.global__nav__list').classList.toggle('active');
+    document.querySelector('.global__nav').classList.toggle('active');
     });
+
+    $('.mv').slick({
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        speed: 1200,
+        arrows: false,
+        swipe: false,
+    })
+
+    window.addEventListener('load', () =>{
+        AOS.init();
+    })
